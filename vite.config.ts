b9f29f83284e85c15fa460aa4af28c2f4139bbfa
blessingndeks <blessinggabbie21@gmail.com>
2026-04-10 15:24:37 +0100

    base settings

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/GlobeBites/",
   server: {
     host: "::",
     port: 8080,
